2dd02f24287f8377cf481a52fa2349eb067fb032
alxss01 <alex.estudos2024@gmail.com>
2025-04-12 15:10:16 -0300

    adiciona arquivos de manifestos nodejs e pasta com rota pública da aplicação

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+function Home() {
+    return (
+        <>
+            <h1>Liberdade Financeira.</h1>
+            <ul>
+                <li>Criado repositório GitHub</li>
+                <li>Clone na máquina local</li>
+                <li>.nvmrc com lts/hydrogen</li>
+                <li>npm init (Licença MIT)</li>
+                <li>npm install next react react-dom</li>
+                <li>Criado pasta pages/index.js com function Home</li>
+                <li>script "dev": "next dev" no package.json</li>
+                <li>Configurado rota pública pelo VSCode</li>
+                <li>.gitignore com .next e node_modules</li>
+                <li>git add, git commit, git push</li>
+                <li>Importado repositório na Vercel</li>
+                <li>Deploy na Vercel, disponibilização da rota pública</li>
+
+            </ul>
+        </>
+    )
+}
+
+export default Home;
